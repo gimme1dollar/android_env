5e5bdb33d327344f5025547140b69dbd122420af
JYLee <gimme1dollar@postech.ac.kr>
2023-12-26 16:28:06 +0900

    -read-only flag on launch

--- android_env/components/simulators/emulator/emulator_launcher.py
+++ android_env/components/simulators/emulator/emulator_launcher.py
@@ -151,9 +151,11 @@ class EmulatorLauncher:
         '-no-audio',
         '-show-kernel',
         '-verbose',
+        '-read-only',
         '-avd',
         self._avd_name,
     ] + grpc_port + run_headless + ports + snapshot + network_args
+    print(command)
     logging.info('Emulator launch command: %s', ' '.join(command))
     # Prepare logfile.
     self._emulator_output = open(self._logfile_path, 'wb')
